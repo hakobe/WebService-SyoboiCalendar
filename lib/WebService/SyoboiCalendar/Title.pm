a7f90957626941db47312eca5a6feca1394f626b
hakobe <hakobe@gmail.com>
2011-09-06 21:30:22 +0900

    Titleに便利なメソッドを幾つか追加

--- lib/WebService/SyoboiCalendar/Title.pm
+++ lib/WebService/SyoboiCalendar/Title.pm
@@ -2,6 +2,8 @@ package WebService::SyoboiCalendar::Title;
 use Mouse;
 use Smart::Args;
 use utf8;
+use Regexp::Common qw /URI/;
+use Encode;
 
 has api_result => (
     is => 'rw',
@@ -21,8 +23,8 @@ sub BUILD {
             my $cast = {};
             my @lines = (split /\r\n/, $part);
             for my $line (@lines[1..$#lines]) {
-                my (undef, $character, $voice_actor) = split /:/, $line;
-                $cast->{$character} = $voice_actor;
+                my (undef, $character, $voice_actor) = split /:/, decode_utf8($line);
+                $cast->{$character} = $voice_actor if $character && $voice_actor;
             }
             $self->cast($cast);
         }
@@ -32,6 +34,17 @@ sub BUILD {
 sub tid {
     args my $self;
     $self->api_result->{TID};
+}
+
+sub urls {
+    args my $self;
+    my @urls = $self->api_result->{Comment} =~ m/$RE{URI}{HTTP}/g;
+    \@urls;
+}
+
+sub official_site_url {
+    args my $self;
+    $self->urls->[0];
 }
 
 sub title {
@@ -46,6 +59,12 @@ sub sub_titles {
     [split /\r\n/, $sub_titles];
 }
 
+sub sub_title {
+    args_pos my $self, my $count;
+    my $sub_titles = $self->sub_titles;
+    $sub_titles && $sub_titles->[$count - 1];
+}
+
 sub characters {
     args my $self;
     [keys %{ $self->cast }];
@@ -58,6 +77,7 @@ sub voice_actors {
 
 sub find_cast {
     args_pos my $self, my $character;
+    $character = decode_utf8($character); # XXX
 
     my ($result) = map { $self->cast->{$_} } grep {
         $_ =~ m/$character/;
